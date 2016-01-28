44d9fcd1ea6143fd5fe711794a8f82c0c3881e91
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2016-01-27 22:32:27 -0500

    Initial commit

---
+++ app/src/main/java/com/jfsaaved/piccarta/main/Main.java
@@ -0,0 +1,46 @@
+package com.jfsaaved.piccarta.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 343076 on 27/01/2016.
+ */
+public class Main extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
