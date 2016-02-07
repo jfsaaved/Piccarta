0a6f974b96d03bcd458900e8e4f76c607dd41907
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2016-02-06 23:40:27 -0500

    February 6, 2016 - Structuring
    Figuring out the code structure
    
    TODO:
    - Structuring

--- app/src/main/java/com/jfsaaved/piccarta/activities/LoginActivity.java
+++ app/src/main/java/com/jfsaaved/piccarta/activities/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.jfsaaved.piccarta.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.jfsaaved.piccarta.R;
 
 /**
  * Created by 343076 on 04/02/2016.
@@ -10,7 +16,10 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
     }
 
